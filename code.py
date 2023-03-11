bfafd608348024009bbdcdea2d875e4f9cd1ddd0
AmanBartwal <126442039+AmanBartwal@users.noreply.github.com>
2023-03-11 11:06:46 +0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,7 @@
+dark = input("enter your name: ")
+print( "hey " + dark + " you want to know your bmi . ")
+num1 = float (input("enter your weight(Kg): "))
+num2 = float(input("enter your height(meter): "))
+ken = num1 / (num2*num2)
+print("your Bmi is " + str(float(ken)) +".")
+
